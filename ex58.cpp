c5b895b972876c9e966a3bd784754aee8ac90ab9
Finem50 <Finem50@live.com>
2018-09-07 00:19:07 -0500

    Implemented exercise to manipulate character case (a - A)

---
+++ ex58.cpp
@@ -0,0 +1,34 @@
+//P54.cpp - This program reads one character from the keyboard and will
+//convert it to uppercase, if it is lowercase
+//convert it to lowercase, if it is uppercase
+//display a message, if it is a digit
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+
+  char c;
+
+  cout << "Enter a character: \n";
+  cin >> c;
+
+  if(isalpha(c)){ //Check to see if it is a letter of alphabet
+
+    if(isupper(c)){ //check to see if it is uppercase
+      c = tolower(c);
+      cout << "Your character " << c << "is in uppercase.";
+      cout << "Its lowercase case is " << c << endl;
+    }else {
+      c = toupper(c);
+      cout << "Your character " << c << " is in lowercase.";
+      cout << "Its uppercase is " << c << endl;
+    }
+  }else {
+    cout << "Your character " << c << " is a digit. \n";
+  }
+
+  return 0;
+}
