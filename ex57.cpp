d9a46b1ad5122acaa8e8896946da0e57c1c5fc84
Finem50 <Finem50@live.com>
2018-09-07 00:02:31 -0500

    Implemented usage of .eof() for reading to end of file instead of previous method

---
+++ ex57.cpp
@@ -0,0 +1,44 @@
+//P54.cpp - This program reads the entire contents of an input file and will
+//display it with the same format.
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+void get_input_stream(ifstream& in_s);
+
+int main(){
+
+  char c, next_char;
+  ifstream in_s; //Declaration of the stream of type input
+
+  get_input_stream(in_s);
+  in_s.get(next_char);
+
+  cout << "Here are the entire contents of the input file \n\n";
+
+  while (! in_s.eof()){ //Read all numbers one-by-one to the end of the file
+    cout.put(next_char);
+    in_s.get(next_char);
+  }
+
+  cout << "\n I am done with writing the contents of the file \n\n";
+
+  in_s.close(); //Close the connection (file)
+
+  return 0;
+}
+
+void get_input_stream(ifstream& in_s){
+
+  char input_file[15];
+  cout << "Please input the input file name \n"; //Get the file name
+  cin >> input_file;
+
+  in_s.open(input_file); //Connect to the input file and test
+  if(in_s.fail()){
+    cout << "Input file opening failed. \n";
+    exit(1); //If we couldn't open the file to read from we exit
+  }
+}
