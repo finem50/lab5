9a41d40fd64bd97dc62e56e7f7b50ab46b1ac751
Finem50 <Finem50@live.com>
2018-09-10 21:22:07 -0500

    Progress on text-to-HTML converter

---
+++ post5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void instructions();
+void get_test_files(ifstream& in_s, ofstream& out_s);
+void write_header(ofstream& out_s);
+void write_footer(ofstream& out_s);
+void copy_test(ifstream& in_s, ofstream& out_s);
+
+int main(){
+
+
+}
+
+void instructions(){
+
+
+}
+
+void get_test_files(ifstream& in_s, ofstream& out_s){
+
+
+}
+
+void write_header(ofstream& out_s){
+
+
+}
+
+void write_footer(ofstream& out_s){
+
+
+}
+
+void copy_test(ifstream& in_s, ofstream& out_s){
+
+  
+}
